17566f3b5e312d0770d8beab7068c39445ed2015
Danshil Kokil Mungur <me@danshilm.com>
2022-06-20 02:40:53 +0400

    feat: add twrnc

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,12 +1,12 @@
 import { StyleSheet } from 'react-native';
-
+import tw from 'twrnc';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../navigation/types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
+    <View style={tw`flex flex-1 justify-center items-center `}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/src/screens/TabOneScreen.tsx" />
