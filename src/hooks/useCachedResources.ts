91cf0545b34ac8e6a14f3ee0f7da350f318d8987
Danshil Kokil Mungur <me@danshilm.com>
2022-06-21 13:16:29 +0400

    chore(tw): clarify loading custom fonts

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -33,19 +33,19 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...FontAwesome.font,
-          'space-mono': SpaceMono_400Regular,
-          'space-mono-italic': SpaceMono_400Regular_Italic,
-          'space-mono-bold': SpaceMono_700Bold,
-          'space-mono-bold-italic': SpaceMono_700Bold_Italic,
-          'inter-thin': Inter_100Thin,
-          'inter-extralight': Inter_200ExtraLight,
-          'inter-light': Inter_300Light,
-          inter: Inter_400Regular,
-          'inter-medium': Inter_500Medium,
-          'inter-semibold': Inter_600SemiBold,
-          'inter-bold': Inter_700Bold,
-          'inter-extrabold': Inter_800ExtraBold,
-          'inter-black': Inter_900Black,
+          SpaceMono_400Regular,
+          SpaceMono_400Regular_Italic,
+          SpaceMono_700Bold,
+          SpaceMono_700Bold_Italic,
+          Inter_100Thin,
+          Inter_200ExtraLight,
+          Inter_300Light,
+          Inter_400Regular,
+          Inter_500Medium,
+          Inter_600SemiBold,
+          Inter_700Bold,
+          Inter_800ExtraBold,
+          Inter_900Black,
         });
 
         // await clear();
