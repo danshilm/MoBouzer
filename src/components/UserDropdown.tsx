e8c13b24381b58de997c7a9ad5eeda130ee13822
Danshil Kokil Mungur <me@danshilm.com>
2022-07-05 20:49:18 +0400

    feat(user): add profile picture fallback

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -1,4 +1,4 @@
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
@@ -12,11 +12,20 @@ export default function UserDropdown() {
   return (
     <>
       <View style={tw`flex justify-center h-full ml-1`}>
-        <TouchableOpacity activeOpacity={0.7} onPress={() => setisOpen(!isOpen)}>
-          <Image
-            source={user ? { uri: user?.photoURL } : require('../../assets/images/icon.png')}
-            style={tw`border border-gray-300 rounded-md w-7.5 h-7.5`}
-          />
+        <TouchableOpacity
+          style={tw`border border-gray-300 rounded-md w-7.5 h-7.5 flex items-center justify-center`}
+          activeOpacity={0.7}
+          onPress={() => setisOpen(!isOpen)}
+        >
+          {user?.photoURL ? (
+            <Image
+              source={{ uri: user.photoURL }}
+              defaultSource={require('../../assets/images/person.png')}
+              style={tw`w-7.5 h-7.5 rounded-md`}
+            />
+          ) : (
+            <Ionicons name="person" size={20} color={tw.color('gray-500')} />
+          )}
         </TouchableOpacity>
       </View>
       {isOpen && (
