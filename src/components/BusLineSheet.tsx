84e581aa1cc4a931117e6cd55bc1b07c25e0d26f
Danshil Kokil Mungur <me@danshilm.com>
2022-07-17 23:49:34 +0400

    feat: use @gorhom/bottom-sheet

--- src/components/BusLineSheet.tsx
+++ src/components/BusLineSheet.tsx
@@ -1,22 +1,41 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useMemo, useRef } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import Animated from 'react-native-reanimated';
-import { BusLineWithStops } from '../interfaces/busline';
+import { BusLineWithStops, BusStopWithOrder } from '../interfaces/busline';
 import tw from '../lib/tailwind';
-import BottomSheet from './BottomSheet';
 import BusLineStopCard from './BusLineStopCard';
 
 export default function BusLineSheet({ data }: { data: BusLineWithStops }) {
+  const sheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => [68, 250, 500], []);
+  const maxOrder = data.busStops[data.busStops.length - 1].order;
+
+  const renderItem = useCallback(
+    (data: BusStopWithOrder) => (
+      <BusLineStopCard data={data} maxOrder={maxOrder} key={data.order} />
+    ),
+    []
+  );
+
+  const HandleComponent = () => {
+    return (
+      <View style={tw`items-center w-full`}>
+        <View style={tw`w-8 h-1 mt-1 bg-gray-300 rounded-md`} />
+      </View>
+    );
+  };
+
   return (
-    <BottomSheet minSheetHeight={36 + 36} maxSheetHeight={256}>
+    <BottomSheet
+      snapPoints={snapPoints}
+      ref={sheetRef}
+      index={0}
+      handleComponent={(props) => <HandleComponent />}
+    >
       {/* Header */}
-      <Animated.View style={tw`h-[18] rounded-[10px]`}>
-        {/* Pill */}
-        <View style={tw`items-center w-full`}>
-          <View style={tw`w-8 h-1 mt-1 bg-gray-300 rounded-md`} />
-        </View>
+      <View style={tw`h-[15] rounded-[10px]`}>
         <View style={tw`flex-row items-center mx-2`}>
           <View style={tw`h-15 w-17 items-center justify-center`}>
             <TouchableOpacity style={tw`items-center justify-center w-8 h-8`}>
@@ -25,24 +44,12 @@ export default function BusLineSheet({ data }: { data: BusLineWithStops }) {
           </View>
           <Text style={tw`text-lg font-inter-medium`}>Bus Line {data.id}</Text>
         </View>
-      </Animated.View>
+      </View>
       {/* Divider */}
       <View style={tw`h-px bg-gray-600 mx-3`} />
-      {/* TODO fix unable to scroll on Android */}
-      {/* TODO fix scrollview snapping back to top on iOS */}
-      <ScrollView
-        scrollEventThrottle={16}
-        style={tw`flex-1`}
-        // onScroll={() => console.log('scrolling')}
-      >
-        {data.busStops.map((busStop) => (
-          <BusLineStopCard
-            data={busStop}
-            maxOrder={data.busStops[data.busStops.length - 1].order}
-            key={busStop.order}
-          />
-        ))}
-      </ScrollView>
+      <BottomSheetScrollView contentContainerStyle={tw``} focusHook={useFocusEffect}>
+        {data.busStops.map(renderItem)}
+      </BottomSheetScrollView>
     </BottomSheet>
   );
 }
