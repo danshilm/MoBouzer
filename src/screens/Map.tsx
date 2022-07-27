00b4da158b75a40a6a78b18ce278d9e62767689e
Danshil Kokil Mungur <me@danshilm.com>
2022-07-28 01:54:24 +0400

    feat(map): use react-native-clusterer

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,29 +1,43 @@
-import React, { useCallback, useRef } from 'react';
-import { Text } from 'react-native';
+import React, { useCallback, useRef, useState } from 'react';
+import { Text, useWindowDimensions } from 'react-native';
+import { useClusterer } from 'react-native-clusterer';
 import MapView, { Callout, MapEvent, Marker } from 'react-native-maps';
 import { getBusStops } from '../api/firestore';
 import ViewWithSearchBar from '../components/SearchBar/ViewWithSearchBar';
 import regionCoordinates from '../constants/Map';
-import { NodeElement } from '../interfaces/common';
 import tw from '../lib/tailwind';
 
 export default function Map() {
+  const [region, setRegion] = useState(regionCoordinates);
   const mapRef = useRef<MapView | null>(null);
   const allBusStops = getBusStops();
+  const { width: mapWidth, height: mapHeight } = useWindowDimensions();
+  const [points] = useClusterer(
+    allBusStops.map((busStop) => ({
+      type: 'Feature',
+      properties: { name: busStop.tags?.name, id: busStop.id },
+      geometry: { type: 'Point', coordinates: [busStop.lon, busStop.lat] },
+    })),
+    { width: mapWidth, height: mapHeight },
+    region
+  );
 
-  const renderItem = useCallback((data: NodeElement) => {
+  const renderItem = useCallback((point) => {
     return (
       <Marker
-        coordinate={{ latitude: data.lat, longitude: data.lon }}
-        key={data.id}
+        coordinate={{
+          latitude: point.geometry.coordinate[0],
+          longitude: point.geometry.coordinate[1],
+        }}
+        key={point.id}
         pinColor="tomato"
         tracksViewChanges={false}
         // buggy on ios
         // image={require('../../assets/images/location.png')}
       >
-        {data.tags && data.tags.name && (
+        {point.properties.name && (
           <Callout tooltip={false}>
-            <Text>{data.tags['name'] as string}</Text>
+            <Text>{point.properties.name}</Text>
           </Callout>
         )}
       </Marker>
@@ -54,6 +68,7 @@ export default function Map() {
           longitudeDelta: regionCoordinates.longitudeDelta,
         }}
         ref={mapRef}
+        onRegionChange={setRegion}
         onPoiClick={handleMarkerPress}
         onMarkerPress={handleMarkerPress}
         toolbarEnabled={false}
@@ -61,7 +76,7 @@ export default function Map() {
         loadingEnabled={true}
         moveOnMarkerPress={false}
       >
-        {allBusStops.map(renderItem)}
+        {points.map((point) => renderItem(point))}
       </MapView>
 
       {/* use this as reference to put a compass and trigger follow user location */}
