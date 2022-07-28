b41376a8321125846d743a89023cad251176f19d
Danshil Kokil Mungur <me@danshilm.com>
2022-07-28 18:18:58 +0400

    chore: cleanup

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,26 +1,15 @@
 import type * as GeoJSON from 'geojson';
 import React, { useCallback, useRef, useState } from 'react';
-import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import { useWindowDimensions } from 'react-native';
 import type { supercluster } from 'react-native-clusterer';
 import { useClusterer } from 'react-native-clusterer';
-import { Animated as MapView, Callout, MapEvent, Marker } from 'react-native-maps';
+import MapView, { MapEvent, Marker } from 'react-native-maps';
 import { getBusStops } from '../api/firestore';
 import ViewWithSearchBar from '../components/SearchBar/ViewWithSearchBar';
 import regionCoordinates from '../constants/Map';
 import tw from '../lib/tailwind';
 
 export default function Map() {
-  // const getDataPoint = useCallback((busStop: NodeElement) => {
-  //   return {
-  //     type: 'Feature',
-  //     properties: { name: busStop.tags?.name, id: busStop.id },
-  //     geometry: { type: 'Point', coordinates: [busStop.lon, busStop.lat] },
-  //   } as
-  //     | supercluster.PointFeature<GeoJSON.GeoJsonProperties>
-  //     | supercluster.ClusterFeatureClusterer<GeoJSON.GeoJsonProperties>;
-  // }, []);
-
-  // const region = useMemo(() => new AnimatedRegion(regionCoordinates), []);
   const [region, setRegion] = useState(regionCoordinates);
   const mapRef = useRef<MapView | null>(null);
   const allBusStops = getBusStops();
@@ -57,22 +46,7 @@ export default function Map() {
           tracksViewChanges={false}
           // buggy on ios
           // image={require('../../assets/images/location.png')}
-        >
-          {point.properties?.cluster ? (
-            // Render Cluster
-            <View style={styles.clusterMarker}>
-              <Text style={styles.clusterMarkerText}>{point.properties.point_count}</Text>
-            </View>
-          ) : (
-            // Else, use default behavior to render
-            // a marker and add a callout to it
-            <Callout>
-              <View style={styles.calloutContainer}>
-                <Text>{JSON.stringify(point.properties)}</Text>
-              </View>
-            </Callout>
-          )}
-        </Marker>
+        />
       );
     },
     []
@@ -123,25 +97,3 @@ export default function Map() {
     </ViewWithSearchBar>
   );
 }
-
-const styles = StyleSheet.create({
-  calloutContainer: {
-    width: 200,
-    height: 200,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 10,
-  },
-  clusterMarker: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: '#8eb3ed',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  clusterMarkerText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-});
