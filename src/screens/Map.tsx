06bdace5f6ca130951bb2e992ef3a53927d989d9
Danshil Kokil Mungur <me@danshilm.com>
2022-07-28 18:16:05 +0400

    feat(map): fine tune clustering

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,13 +1,26 @@
+import type * as GeoJSON from 'geojson';
 import React, { useCallback, useRef, useState } from 'react';
-import { Text, useWindowDimensions } from 'react-native';
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import type { supercluster } from 'react-native-clusterer';
 import { useClusterer } from 'react-native-clusterer';
-import MapView, { Callout, MapEvent, Marker } from 'react-native-maps';
+import { Animated as MapView, Callout, MapEvent, Marker } from 'react-native-maps';
 import { getBusStops } from '../api/firestore';
 import ViewWithSearchBar from '../components/SearchBar/ViewWithSearchBar';
 import regionCoordinates from '../constants/Map';
 import tw from '../lib/tailwind';
 
 export default function Map() {
+  // const getDataPoint = useCallback((busStop: NodeElement) => {
+  //   return {
+  //     type: 'Feature',
+  //     properties: { name: busStop.tags?.name, id: busStop.id },
+  //     geometry: { type: 'Point', coordinates: [busStop.lon, busStop.lat] },
+  //   } as
+  //     | supercluster.PointFeature<GeoJSON.GeoJsonProperties>
+  //     | supercluster.ClusterFeatureClusterer<GeoJSON.GeoJsonProperties>;
+  // }, []);
+
+  // const region = useMemo(() => new AnimatedRegion(regionCoordinates), []);
   const [region, setRegion] = useState(regionCoordinates);
   const mapRef = useRef<MapView | null>(null);
   const allBusStops = getBusStops();
@@ -19,30 +32,51 @@ export default function Map() {
       geometry: { type: 'Point', coordinates: [busStop.lon, busStop.lat] },
     })),
     { width: mapWidth, height: mapHeight },
-    region
+    region,
+    {
+      minPoints: 5,
+      extent: 768,
+      radius: 18,
+    }
   );
 
-  const renderItem = useCallback((point) => {
-    return (
-      <Marker
-        coordinate={{
-          latitude: point.geometry.coordinate[0],
-          longitude: point.geometry.coordinate[1],
-        }}
-        key={point.id}
-        pinColor="tomato"
-        tracksViewChanges={false}
-        // buggy on ios
-        // image={require('../../assets/images/location.png')}
-      >
-        {point.properties.name && (
-          <Callout tooltip={false}>
-            <Text>{point.properties.name}</Text>
-          </Callout>
-        )}
-      </Marker>
-    );
-  }, []);
+  const renderItem = useCallback(
+    (
+      point:
+        | supercluster.PointFeature<GeoJSON.GeoJsonProperties>
+        | supercluster.ClusterFeatureClusterer<GeoJSON.GeoJsonProperties>
+    ) => {
+      return (
+        <Marker
+          coordinate={{
+            latitude: point.geometry.coordinates[1],
+            longitude: point.geometry.coordinates[0],
+          }}
+          key={point.properties?.cluster_id ?? `point-${point.properties?.id}`}
+          pinColor="tomato"
+          tracksViewChanges={false}
+          // buggy on ios
+          // image={require('../../assets/images/location.png')}
+        >
+          {point.properties?.cluster ? (
+            // Render Cluster
+            <View style={styles.clusterMarker}>
+              <Text style={styles.clusterMarkerText}>{point.properties.point_count}</Text>
+            </View>
+          ) : (
+            // Else, use default behavior to render
+            // a marker and add a callout to it
+            <Callout>
+              <View style={styles.calloutContainer}>
+                <Text>{JSON.stringify(point.properties)}</Text>
+              </View>
+            </Callout>
+          )}
+        </Marker>
+      );
+    },
+    []
+  );
 
   const handleMarkerPress = (e: MapEvent) => {
     mapRef.current?.animateToRegion(
@@ -68,7 +102,7 @@ export default function Map() {
           longitudeDelta: regionCoordinates.longitudeDelta,
         }}
         ref={mapRef}
-        onRegionChange={setRegion}
+        onRegionChangeComplete={setRegion}
         onPoiClick={handleMarkerPress}
         onMarkerPress={handleMarkerPress}
         toolbarEnabled={false}
@@ -76,7 +110,7 @@ export default function Map() {
         loadingEnabled={true}
         moveOnMarkerPress={false}
       >
-        {points.map((point) => renderItem(point))}
+        {points.map(renderItem)}
       </MapView>
 
       {/* use this as reference to put a compass and trigger follow user location */}
@@ -89,3 +123,25 @@ export default function Map() {
     </ViewWithSearchBar>
   );
 }
+
+const styles = StyleSheet.create({
+  calloutContainer: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 10,
+  },
+  clusterMarker: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#8eb3ed',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  clusterMarkerText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
