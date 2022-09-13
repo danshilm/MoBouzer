ca25ff5d15f531627a3f7544bfb36ff6335aaa1f
Danshil Kokil Mungur <me@danshilm.com>
2022-09-14 01:19:59 +0400

    [app] fix(maps): remove unused constants + fix default coords

--- apps/mobile-app/src/constants/Map.ts
+++ apps/mobile-app/src/constants/Map.ts
@@ -1,23 +1,14 @@
 import type { CameraStop } from '@rnmapbox/maps';
-
 // latitude = y-axis
 // longitude = x-axis
-// latitude, longitude
-const southWestCoordinates = [-20.525305, 57.307921];
-const northEastCoordinates = [-19.982901, 57.806426];
-const centreCoordinates = [-20.254103, 57.5571735];
-
-export const initialRegion = {
-  /** y-axis */
-  latitude: centreCoordinates[0],
-  /** x-axis */
-  longitude: centreCoordinates[1],
-  latitudeDelta: northEastCoordinates[0] - southWestCoordinates[0],
-  longitudeDelta: (northEastCoordinates[1] - southWestCoordinates[1]) * 1.2,
-};
+// longitude, latitude
+export const southWestCoordinates = [57.307921, -20.525305];
+export const northEastCoordinates = [57.806426, -19.982901];
+export const centreCoordinates = [57.558065, -20.241753];
 
 export const cameraDefaultSettings: CameraStop = {
+  heading: 0,
   animationDuration: 1000,
-  centerCoordinate: [57.558065, -20.241753],
+  centerCoordinate: centreCoordinates,
   zoomLevel: 8.7,
 };
