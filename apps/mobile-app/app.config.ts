38cadc048dea0bcb5f70ee48a13ae0d81a32c501
Danshil Kokil Mungur <me@danshilm.com>
2023-02-26 19:47:55 +0400

    [app] chore(build): upgrade to expo sdk 48

--- apps/mobile-app/app.config.ts
+++ apps/mobile-app/app.config.ts
@@ -30,6 +30,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       googleServicesFile: process.env.GOOGLE_SERVICES_IOS_FILE,
       bundleIdentifier: config.ios?.bundleIdentifier,
       buildNumber: config.ios?.buildNumber,
+      jsEngine: 'jsc',
     },
     android: {
       adaptiveIcon: {
@@ -39,6 +40,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       googleServicesFile: process.env.GOOGLE_SERVICES_ANDROID_FILE,
       package: config.android?.package,
       versionCode: config.android?.versionCode,
+      jsEngine: 'jsc',
     },
     web: {
       favicon: './assets/images/favicon.png',
@@ -60,7 +62,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       firebaseiOSClientId: process.env.FIREBASE_IOS_CLIENT_ID,
       mapboxToken: process.env.RNMAPBOX_TOKEN,
       sentryDsn: process.env.SENTRY_DSN,
-      buildProfile: process.env.APP_ENV
+      buildProfile: process.env.APP_ENV,
     },
     plugins: [
       [
