8b62eb377b366b1188827eb04423c22fb9872656
Danshil Kokil Mungur <me@danshilm.com>
2025-06-01 00:44:13 +0400

    chore(build): replace deprecated property in tsconfig + small fix

--- apps/mobile-app/src/navigation/utils.tsx
+++ apps/mobile-app/src/navigation/utils.tsx
@@ -8,6 +8,7 @@ export const navigationRef = createNavigationContainerRef<RootStackParamList>();
  * prop. If you have access to the navigation prop, do not use this.
  * More info: https://reactnavigation.org/docs/navigating-without-navigation-prop/
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function navigate(name: any, params: any) {
   if (navigationRef.isReady()) {
     navigationRef.navigate(name, params);
